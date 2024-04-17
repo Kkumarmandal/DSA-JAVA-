39c9f1909213530335d58d817d32538173636809
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-04-17 13:01:23 +0530

    Add files via upload

---
+++ 15. Linked list/DoublyLL.java
@@ -0,0 +1,83 @@
+public class DoublyLL {
+
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node (int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+    // Add First in doubly LinkedList
+    public void addFirst (int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;            
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    // Print in doubly LinkedList
+    public void print(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;            
+        }
+        System.out.println("null");
+    }
+    
+    // Remove first
+    public int removeFirst (){
+        if (head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;            
+        }
+
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;            
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+
+    }
+
+    public static void main(String[] args) {
+
+        DoublyLL dll = new DoublyLL();
+
+        // add first
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        
+        dll.print();
+        System.out.println("Add First size " +dll.size);
+
+        // Remove First
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Remove first size " + dll.size);
+
+    }
+    
+}
