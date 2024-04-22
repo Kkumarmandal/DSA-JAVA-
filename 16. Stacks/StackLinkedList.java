1da0a2d6cedf0afd316ddb63c08ebc9132d06a6c
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-04-22 11:58:26 +0530

    Add files via upload

---
+++ 16. Stacks/StackLinkedList.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class StackLinkedList {
+   
+    /*static class Node {
+        int data;
+        Node next;
+
+        Node (int data){
+            this.data = data;
+            this.next = null;
+        }       
+    }
+
+    static class Stack {
+        static Node head = null;
+        
+        //Is Empty
+        public static boolean isEmpty (){
+            return head == null;
+        }
+
+        // Push
+        public static void push (int data){
+            Node newNode = new Node (data);
+
+            if (isEmpty()) {
+                head = newNode;
+                return;                
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // Pop
+        public static int pop (){
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        //Peek
+        public static int peek (){
+            if (isEmpty()) {
+                return -1;                    
+            }
+            return head.data;
+        }       
+        
+    }*/    
+
+    public static void main(String[] args) {
+        //Stack s = new Stack();
+
+        Stack <Integer> s = new Stack<>();  //Collection Framework
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+            
+        }
+        
+    }
+}
---
+++ 16. Stacks/StackRecussion.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+//Push At the Bottom of stack using recurssion O(n)
+public class StackRecussion {
+   
+    public static void pushAtBottom (Stack<Integer>s, int data){
+        //Base Case
+        if (s.isEmpty()) {
+            s.push(data);
+            return;                
+        }
+
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }  
+
+    
+    public static void main(String[] args) {
+        Stack <Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(s, 4);
+
+       
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+            
+        }
+
+
+        
+    }
+}
