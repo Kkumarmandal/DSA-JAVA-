c1bf19d6c2f90959ce55fc3f372d743bb9465144
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-04-25 15:08:26 +0530

    Add files via upload

---
+++ 16. Stacks/reverseString.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class reverseString {
+    public static String reverseString (String str){ //str = "abc"
+        Stack <Character> s = new Stack<>();
+        int idx = 0; 
+
+        while (idx < str.length()) {
+            s.push(str.charAt(idx)); // Stack me push kar dege
+            idx++;            
+        }
+
+        StringBuilder result = new StringBuilder(""); //intilize with empty 
+        while (!s.isEmpty()) {
+            char curr = s.pop(); // chara ko bahar nikalna 
+            result.append(curr); // resul pe store kar dege         
+        }
+        return result.toString(); // string builder ka datatype alag rehta hai toh .tostring        
+
+    }
+
+    public static void main(String[] args) {
+        String str = "abcd";
+        System.out.println( "String = " + str);
+
+        String result = reverseString(str);
+        System.out.println("Reverse String = " + result); //"dcba"
+        
+    }
+}
