acb1191544bdfe1540ff3185903fa44c1c44f17d
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-05-05 17:10:11 +0530

    Add files via upload

---
+++ 18. Greedy Algorithm/MaxLengthChainOfPairs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class MaxLengthChainOfPairs {
+    public static void main(String[] args) {
+        int pairs [] [] = {{5, 24}, {39, 60}, {5, 28}, {27, 40}, {50, 90}};
+
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+        int chainLen = 1;
+
+        int chainEnd = pairs [0][1];
+
+        for (int i = 0; i < pairs.length; i++){
+            if (pairs[i][0] > chainEnd) {
+                chainLen++;
+                chainEnd = pairs[i][1];
+                
+            }
+        }
+        System.out.println("Maximum Length of Chain = " + chainLen);
+        
+    }
+}
