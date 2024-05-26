e6a54f6912c2ad3b8e42af862d9c99a7d0ade82b
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-05-26 11:33:45 +0530

    Hashing Operation

---
+++ 22. Hashing/HashMapOperaation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class HashMapOperaation {
+    public static void main(String[] args) {
+        HashMap <String, Integer> hm = new HashMap<>();
+
+        //Insert -> O(1) -> undifine Order follow
+        hm.put("India", 150);
+        hm.put("China", 100);
+        hm.put("Us", 50);
+
+        //Print
+        System.out.println(hm);
+
+
+        
+    }
+}
