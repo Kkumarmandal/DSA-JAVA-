1146c1dc1c5d7382d982c70c7924c6f864b4ba05
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-05-29 12:44:32 +0530

    Count Distinct Element -> O(n)

---
+++ 22. Hashing/CountDistinctElement.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CountDistinctElement {
+    public static void main(String[] args) {
+        int num [] = {4, 3, 2, 5, 6, 7, 3, 4, 2, 1};
+
+        HashSet <Integer> set = new HashSet<>();
+
+        for (int i = 0; i < num.length; i++){
+            set.add(num[i]);
+        }
+        System.out.println("nums size = " + set.size());
+        
+    }
+}
