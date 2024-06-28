2451c06028be6175c8adea7395d997c70863bd0e
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-06-28 10:25:02 +0530

    Knapsack Tabulation

--- 25. Dyanamic Programming/Knapsack.java
+++ 25. Dyanamic Programming/Knapsack.java
@@ -34,6 +34,46 @@ public class Knapsack {
             return dp [n][W];
         }
     }
+
+    public static void print (int dp [][]){
+        for (int i = 0; i < dp.length; i++){
+            for (int j = 0; j < dp[0].length; j++){
+                System.out.print(dp [i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static int KnapsackTab (int val [], int wt [], int W){
+        int n = val.length;
+        int dp [][] = new int[n+1][W+1];
+        
+        for (int i = 0; i < dp.length; i++){ // initilize 0th col
+            dp [i][0] = 0;
+        }
+        for (int j = 0; j < dp[0].length; j++){ // initilize 0th row
+            dp [0][j] = 0;
+        }
+
+        for (int i = 1; i < n+1; i++){
+            for (int j = 1; j < W+1; j++){
+                int v = val[i-1]; //ith item val
+                int w = wt [i-1]; // ith item wt
+                if (w <= j) { //valid
+                    int incProfit = v + dp[i-1][j-w];
+                    int excProfit = dp [i-1][j];
+                    //comparing
+                    dp [i][j] = Math.max(incProfit, excProfit);                    
+                } else { //invalid
+                    int excProfit = dp [i-1][j];
+                    dp [i][j] = excProfit;
+                }
+            }
+        }
+        print(dp);
+        return dp [n][W];
+    }
     public static void main(String[] args) {
         // recursion
         int val [] = {15, 14, 10, 45, 30};
@@ -51,6 +91,21 @@ public class Knapsack {
         System.out.println(knapsackRecursion(val, wt, W, val.length));
 
         System.out.println(knapsakMemoization(val, wt, W, val.length, dp)); //-> 75
+
+        System.out.println(KnapsackTab(val, wt, W));
+
+        /*
+        0 0 0 0 0 0 0 0 
+        0 0 15 15 15 15 15 15
+        0 0 15 15 15 15 15 29
+        0 10 15 25 25 25 25 29
+        0 10 15 45 55 60 70 70
+        0 10 15 45 55 60 70 75
+
+        75
+        */
+
+    }
 
     }
 }
