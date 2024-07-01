28f40937f727d14c79ee60b06bd379b26e05ad19
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-07-01 15:50:22 +0530

    Lowest Common Subsequence Tabulation

--- 25. Dyanamic Programming/LowestCommonSubsequence.java
+++ 25. Dyanamic Programming/LowestCommonSubsequence.java
@@ -34,6 +34,35 @@ public class LowestCommonSubsequence {
         }
 
     }
+
+    public static int lcsTab (String str1, String str2){
+        int n = str1.length();
+        int m = str2.length();
+
+        int dp [][] = new int[n+1][m+1];
+        //initilazation
+        for (int i = 0; i < n+1; i++){
+            for (int j = 0; j < m+1; j++){
+                if (i == 0 || j == 0) {
+                    dp [i][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < n+1; i++){
+            for (int j = 1; j < m+1; j++){
+                if (str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp [i][j] = dp [i-1][j-1]+1;
+                }else {
+                    int ans1 = dp [i-1][j];
+                    int ans2 = dp [i][j-1];
+                    dp [i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
     public static void main(String[] args) {
         String str1 = "abcdge";
         String str2 = "abedg"; // lcs = "abdg" => length = 4;
@@ -52,5 +81,7 @@ public class LowestCommonSubsequence {
         }
 
         System.out.println(lcs2(str1, str2, n, m, dp)); //-> 4
+
+        System.out.println(lcsTab(str1, str2)); //-> 4
     }
 }
