44b5f0e67452e1ed7b26f5c3044a306a58aa41c0
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-07-04 15:34:45 +0530

    Catalan Memoization

--- 25. Dyanamic Programming/CatalanNumber.java
+++ 25. Dyanamic Programming/CatalanNumber.java
@@ -10,10 +10,32 @@ public class CatalanNumber {
         return ans;       
     }
 
+    public static int catalanMemo (int n, int dp []){
+        //base caase
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        if (dp [n] != -1) {
+            return dp [n];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            ans += catalanMemo(i, dp) * catalanMemo(n-i-1, dp);
+        }
+        return dp [n] = ans;
+    }
+
 
     public static void main(String[] args) {
         int n = 4;
 
         System.out.println(catalanRec(n)); // -> 14
+
+        int dp [] = new int[n+1];
+        Arrays.fill(dp, -1);
+
+        System.out.println(catalanMemo(n, dp)); // -> 14
     }
 }
