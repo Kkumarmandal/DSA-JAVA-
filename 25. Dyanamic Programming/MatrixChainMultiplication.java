9c5c6e9ac31c9166aac29dcf303e46788e545053
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-07-06 17:56:40 +0530

    Matrix Chain Multiplication Memoiazation

--- 25. Dyanamic Programming/MatrixChainMultiplication.java
+++ 25. Dyanamic Programming/MatrixChainMultiplication.java
@@ -19,12 +19,36 @@ public class MatrixChainMultiplication {
         return minCost;
     }
 
+    // Matrix Chian Multiplication Memoization
+    public static int mcmMem (int arr [], int i, int j, int dp [][]){
+        //base case
+        if (i == j) {
+            return 0;
+        }
 
+        if (dp [i][j] != -1) {
+            return dp [i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j-1; k++){
+            int cost1 = mcmMem(arr, i, k, dp);
+            int cost2 = mcmMem(arr, k+1, j, dp);
+            int cost3 = arr [i-1] * arr [k] * arr [j];
+            ans = Math.min(ans, cost1 + cost2 + cost3);
+        }
+        return dp [i][j] = ans;
+    }
     
     public static void main(String[] args) {
         int arr [] = {1, 2, 3, 4, 3};
         int n = arr.length;
 
-        System.out.println(mcmrecurss (arr, 1, n-1));
+        //System.out.println(mcmrecurss (arr, 1, n-1)); // 30
+
+        int dp [][] = new int[n][n];
+        for (int i = 0; i < n; i++){
+            Arrays.fill(dp [i], -1);
+        }
+        System.out.println(mcmMem(arr, 1, n-1, dp)); // 30
     }
 }
